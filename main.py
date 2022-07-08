871bfd23f7fe419f1849b0c0cdbc4c5b5c9f0f68
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2022-07-08 12:09:11 -0400

    Removed slowapi

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import os, uvicorn
 from fastapi import FastAPI
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.errors import RateLimitExceeded
-from slowapi.util import get_remote_address
 from rich.console import Console
 
 # --- Constants --- #
 
 app = FastAPI()
-limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 console = Console()
 apps = []
 
